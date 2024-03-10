fcf762e8687fd06528a039f8cce3c5487b23148d
Edson Carlos <edsoncarlos@ymail.com>
2024-03-10 19:37:19 -0300

    test: Testando quando a rota é nula

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -16,5 +16,19 @@ namespace JornadaMilhas.Test
 
             Assert.Equal(validacao, oferta.EhValido);
         }
+
+        [Fact]
+        public void OfertaComRotaNulaTeste()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+            var msgError = "A oferta de viagem não possui rota ou período válidos.";
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains(msgError, oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
     }
 }
